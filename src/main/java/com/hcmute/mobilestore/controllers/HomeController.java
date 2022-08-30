8d67c7db5d487264f1bd36eb3e76f307e74992b5
nht0910201 <huutri0910@gmail.com>
2022-08-30 17:47:09 +0700

    initial commit again

--- src/main/java/com/hcmute/mobilestore/controllers/HomeController.java
+++ src/main/java/com/hcmute/mobilestore/controllers/HomeController.java
@@ -1,8 +1,5 @@
 package com.hcmute.mobilestore.controllers;
 
-import com.hcmute.mobilestore.models.Product;
-import com.hcmute.mobilestore.repository.ProductRepository;
-import com.hcmute.mobilestore.repository.SupplierRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -12,12 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(path = "")
 public class HomeController {
-    @Autowired
-    private ProductRepository productRepository;
     @GetMapping(path = "/Home")
     public String showHomePage(ModelMap modelMap) {
-        Iterable<Product> products=productRepository.findAll();
-        modelMap.addAttribute("products",products);
         return "viewHome/Index";
     }
 }
--- src/main/java/com/hcmute/mobilestore/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hcmute.mobilestore.repository;
-
-import com.hcmute.mobilestore.models.Account;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AccountRepository extends CrudRepository<Account, Integer> {
-    Account findByEmail(String username);
-}
